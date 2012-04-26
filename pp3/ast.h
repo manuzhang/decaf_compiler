b353945e72ccd6da2adf350cb5b44c74bfe3ebac
ManuZhang <OwenZhang1990@gmail.com>
2012-04-26 11:11:22 +0800

    add modified files

--- pp3/ast.h
+++ pp3/ast.h
@@ -20,12 +20,11 @@
  * set up links in both directions. The parent link is typically not used 
  * during parsing, but is more important in later phases.
  *
- * Printing: The only interesting behavior of the node classes for pp2 is the 
- * ability to print the tree using an in-order walk.  Each node class is 
- * responsible for printing itself/children by overriding the virtual 
- * PrintChildren() and GetPrintNameForNode() methods. All the classes we 
- * provide already implement these methods, so your job is to construct the
- * nodes and wire them up during parsing. Once that's done, printing is a snap!
+ * Semantic analysis: For pp3 you are adding "Check" behavior to the ast
+ * node classes. Your semantic analyzer should do an inorder walk on the
+ * parse tree, and when visiting each node, verify the particular
+ * semantic rules that apply to that construct.
+
  */
 
 #ifndef _H_ast
@@ -33,6 +32,8 @@
 
 #include <stdlib.h>   // for NULL
 #include "location.h"
+#include <iostream>
+using namespace std;
 
 class Node  {
   protected:
@@ -47,13 +48,6 @@ class Node  {
     yyltype *GetLocation()   { return location; }
     void SetParent(Node *p)  { parent = p; }
     Node *GetParent()        { return parent; }
-
-    virtual const char *GetPrintNameForNode() = 0;
-    
-    // Print() is deliberately _not_ virtual
-    // subclasses should override PrintChildren() instead
-    void Print(int indentLevel, const char *label = NULL); 
-    virtual void PrintChildren(int indentLevel)  {}
 };
    
 
@@ -64,8 +58,7 @@ class Identifier : public Node
     
   public:
     Identifier(yyltype loc, const char *name);
-    const char *GetPrintNameForNode()   { return "Identifier"; }
-    void PrintChildren(int indentLevel);
+    friend ostream& operator<<(ostream& out, Identifier *id) { return out << id->name; }
 };
 
 
@@ -78,7 +71,6 @@ class Error : public Node
 {
   public:
     Error() : Node() {}
-    const char *GetPrintNameForNode()   { return "Error"; }
 };
 
 
